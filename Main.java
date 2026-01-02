49fee099f2e3fb5fdda14b0cf17b737cfe3ae9ed
minukusunilkumar1910 <minukkusunil425@gmail.com>
2026-01-02 19:48:14 +0530

    02-02-26

--- Main.java
+++ Main.java
@@ -459,6 +459,8 @@ public class Main{
 //     }
 // }
 
+import java.util.Arrays;
+
 public class Main
 {
    public static void main(String[] args) {
@@ -645,16 +647,30 @@ public class Main
 
 // Delete an Element 
 
-int[] arr = {31,24,55,10,20,30,40,50};
+// int[] arr = {31,24,55,10,20,30,40,50};
 
-int pos = 2;
-for(int i=pos+1;i<arr.length;i++)
-{
-  arr[i-1] = arr[i];
-}
-for(int i =0;i<arr.length - 1;i++)
-{
-   System.out.print(arr[i]+" ");
-}
+// int pos = 2;
+// for(int i=pos+1;i<arr.length;i++)
+// {
+//   arr[i-1] = arr[i];
+// }
+// for(int i =0;i<arr.length - 1;i++)
+// {
+//    System.out.print(arr[i]+" ");
+// }
+
+
+int[] nums = {1,2,1};
+int n = nums.length;
+int ans[] = new int[n+n];
+
+ 
+   for(int i=0;i<n;i++)
+   {
+      ans[i] = nums[i];
+      ans[i+n] = nums[i];
+   }
+
+System.out.println(Arrays.toString(ans));
  }
 }  
